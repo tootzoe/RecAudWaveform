57801e7ffec90afb653d1a0cdf4774235cbd69bd
toot_hzf <toot@tootzoe.com>
2024-03-31 20:56:56 +0700

    Initial Commit

---
+++ RecAudWaveform/RecAudWaveformApp.swift
@@ -0,0 +1,23 @@
+//
+//  RecAudWaveformApp.swift
+//  RecAudWaveform
+//
+//  Created by thor on 31/3/24
+//  
+//
+//  Email: toot@tootzoe.com  Tel: +855 69325538 
+//
+//
+
+
+
+import SwiftUI
+
+@main
+struct RecAudWaveformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
